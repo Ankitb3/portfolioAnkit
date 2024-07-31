96829964cdb4ee7b9e951959ba3123bcc71c4b4a
Ankitb3 <bankarankit3@gmail.com>
2024-07-31 14:04:05 +0530

    loading added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import Home from "./Pages/Home"
+import { lazy, Suspense } from "react"
+import Loading from "./components/Loading";
 
+const Home = lazy(() => import('./Pages/Home'));
+const LoadingFallback = () => (
+  <div className="min-h-screen bg-black">
+    <Loading/>
+  </div>
+);
 const App = () => {
   return (
    <div className="text-white">
+    <Suspense fallback={<LoadingFallback/>}>
    <Home/>
+   </Suspense>
    </div>  )
 }
 
